45207ae0720e30fd23f5005ed140fd7f34f9efc5
HomeMac <homemac@HomeMacs-MacBook-Pro.local>
2022-02-01 19:55:51 -0800

    added loop

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+for (i=0 ; i < weeklyExpenseQuestions.length; i++)
+{
+//window.prompt(weeklyExpenseQuestions[i])
+let answer = parseFloat (window.prompt(weeklyExpenseQuestions[i]))
+weeklyExpenses = weeklyExpenses + answer
+}
+for (i=0; i<monthlyExpenseQuestions.length; i++)
+{ 
+    let monthly = parseFloat (window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + monthly
+}
